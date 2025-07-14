e1aa390be7c0a12cb531dcc9b02224eed2482dce
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2025-07-14 17:55:52 -0300

    Fix area spell skull bug when hand is open

---
+++ data/scripts/events/events/secure_mode.lua
@@ -0,0 +1,18 @@
+-- Secure Mode System using EventCallback
+-- Simple secure mode protection for PvP
+
+local ec = Events()
+ec.onTargetCombat = function(self, target)
+    if self and self:isMonster() then
+        return true
+    end
+    
+    if self and self:hasSecureMode() then
+        if target:isPlayer() then
+            return RETURNVALUE_YOUMAYNOTATTACKTHISCREATURE
+        end
+    end
+
+    return RETURNVALUE_NOERROR
+end
+ec:register() 
